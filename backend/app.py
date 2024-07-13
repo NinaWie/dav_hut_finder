c152e4bbae5737af49b09ccf0f9bd67d242fed2e
Nina Wiedemann <wnina@student.ethz.ch>
2024-07-13 13:20:02 +0000

    revert static folder

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ import pandas as pd
 from filtering import filter_huts
 from check_availability import AvailabilityChecker
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../frontend")
 
 CORS(app, origins=["*", "null"])  # allowing any origin as well as localhost (null)
 
